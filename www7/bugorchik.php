7cdb1d800beb8d06351f1af68c6a4d67ec6bc733
Roxees <150244746+Roxees@users.noreply.github.com>
2023-12-26 21:17:35 +0500

    Add files via upload

---
+++ www7/bugorchik.php
@@ -0,0 +1,11 @@
+
+<body background = "img/fon.png">
+</body>
+<center/><h1/>Data has been successfully added to the database.<center/><h1/><br/> 
+Welcome to the 100 Yuristov.<br/>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Choose file for upload <br/>
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload images" name="submit">
+</form>
+ I like only IMG, PNG, and JPEG.
